57a51a22da47881bba6535b81ef901e1b52a71d3
mehdi faraji <mahdifarajideveloper@gmail.com>
2023-06-17 01:53:15 +0330

    added all tailwindcss config needed for project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {},
-  plugins: [],
+  theme: {
+    fontFamily: {
+      light: ["light", "-apple-system", "system-ui", "sans-serif"],
+      medium: ["medium", "-apple-system", "system-ui", "sans-serif"],
+      bold: ["bold", "-apple-system", "system-ui", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        blue: "#4F73D0",
+        darkBlue: "#224DBA",
+        shadowColor: "rgba(0, 0, 0, 0.08)",
+        gray: "#9D9D9D",
+        lightGray: "#E7E7E7",
+        inputBg: "#FDFDFD",
+        black: "#2D2D2D",
+      },
+      boxShadow: {
+        btn: "0px 3px 4px rgba(0, 0, 0, 0.08)",
+        btnHover: "0px 4px 4px rgba(0, 0, 0, 0.15)",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/line-clamp")],
 };
